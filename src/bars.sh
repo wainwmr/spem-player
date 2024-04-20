fd9565da3fb8476e5edda7ee81054cc93fa3b60a
wainwmr <wainwmr@gmail.com>
2024-04-20 21:48:05 +0100

    refactored to include build steps

--- src/bars.sh
+++ src/bars.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-tempfile=$0.temp
-touch $tempfile
+outfile=$2
+touch $outfile
 
-echo "const scorebars = [" > $tempfile
+echo "const scorebars = [" > $outfile
 for i in $1/spem-choir*.svg; do
-  echo -n "  [" >> $tempfile
-  grep -B 2 '<tspan>[0-9]*</tspan>' $i | sed -n 's/^.*translate(\([^,]*\),.*$/\1/p' | sort -n | sed 's/$/, /' | tr -d '\n' >> $tempfile
-  echo "]," >> $tempfile
+  echo -n "  [" >> $outfile
+  grep -B 2 '<tspan>[0-9]*</tspan>' $i | sed -n 's/^.*translate(\([^,]*\),.*$/\1/p' | sort -n | sed 's/$/, /' | tr -d '\n' >> $outfile
+  echo "]," >> $outfile
 done
-echo "];" >> $tempfile
+echo "];" >> $outfile
 
 echo "Copied to clipboard"
-cat $tempfile | pbcopy
-rm $tempfile
+cat $outfile | pbcopy
